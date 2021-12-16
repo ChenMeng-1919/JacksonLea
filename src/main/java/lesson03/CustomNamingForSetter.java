e5dbbd50371dfb588061bd7168fa915acb844809
Meng Chen <chenmeng_1919@163.com>
2021-12-16 22:52:29 +0800

    Jackson修改字段名和自定义命名策略

--- src/main/java/lesson03/CustomNamingForSetter.java
+++ src/main/java/lesson03/CustomNamingForSetter.java
@@ -2,12 +2,14 @@ package lesson03;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 
 /*
  * @author: cm
  * @date: Created in 2021/12/16 22:36
  * @description:
  */
+@Slf4j
 public class CustomNamingForSetter {
     public static void main(String[] args) throws JsonProcessingException {
         AnimalNaming animal = new AnimalNaming();
@@ -17,11 +19,11 @@ public class CustomNamingForSetter {
 
         ObjectMapper mapper = new ObjectMapper();
         mapper.setPropertyNamingStrategy(new AppendPrefixStrategyForSetter());
-        System.out.println(mapper.writeValueAsString(animal));
+        log.info(mapper.writeValueAsString(animal));
 
 //  String jsonString = "{\"_animalName\":\"sam\",\"_animalSex\":1,\"_animalWeight\":100}";
         String jsonString = "{\"animalName\":\"sam\",\"animalSex\":1,\"animalWeight\":100}";
         AnimalNaming animal2 = mapper.readValue(jsonString, AnimalNaming.class);
-        System.out.println(animal2.toString());
+        log.info(animal2.toString());
     }
 }
