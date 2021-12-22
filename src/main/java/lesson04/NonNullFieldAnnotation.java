abf27bd6b8adbaf6a1221739b9f88df4ae52e5a1
cm <chenmeng_1919@163.com>
2021-12-22 14:16:00 +0800

    Jackson忽略null字段的3种方法

--- src/main/java/lesson04/NonNullFieldAnnotation.java
+++ src/main/java/lesson04/NonNullFieldAnnotation.java
@@ -2,18 +2,20 @@ package lesson04;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 
 /*
  * @author: cm
  * @date: Created in 2021/12/16 22:48
  * @description:
  */
+@Slf4j
 public class NonNullFieldAnnotation {
     public static void main(String[] args) throws JsonProcessingException {
         Animal2NonNull2 animal = new Animal2NonNull2();
         animal.setSex(26);
 
         ObjectMapper mapper = new ObjectMapper();
-        System.out.println(mapper.writeValueAsString(animal)); // 不输出weight
+        log.info(mapper.writeValueAsString(animal)); // 不输出weight
     }
 }
